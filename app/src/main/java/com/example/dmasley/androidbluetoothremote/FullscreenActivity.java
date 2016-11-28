0904c800b8ddae57254eda9ed8d427f970a0824c
DmitryMasley <masley.dmitro@gmail.com>
2016-11-28 11:11:22 +0200

    Update BT communication, steering

--- app/src/main/java/com/example/dmasley/androidbluetoothremote/FullscreenActivity.java
+++ app/src/main/java/com/example/dmasley/androidbluetoothremote/FullscreenActivity.java
@@ -1,26 +1,30 @@
 package com.example.dmasley.androidbluetoothremote;
 import com.example.dmasley.androidbluetoothremote.bluetooth.ConnectionCreationThread;
 
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import java.math.BigInteger;
 
-import java.util.Set;
-
 enum STATE {
     NOT_CONNECTED,
     CONNECTING,
     CONNECTED
+}
+enum Direction {
+    FORWARD,
+    BACK
+}
+enum Steering {
+    LEFT,
+    RIGHT,
+    STRAIGHT
 }
 
 /**
@@ -30,6 +34,9 @@ enum STATE {
 public class FullscreenActivity extends AppCompatActivity {
     private BluetoothDevice device;
     private STATE state;
+    private Direction dir = Direction.FORWARD;
+    private int speed = 0;
+    private Steering steer = Steering.STRAIGHT;
     ConnectionCreationThread create;
     FullscreenActivity(){
 
@@ -53,8 +60,8 @@ public class FullscreenActivity extends AppCompatActivity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    Button onButton;
-    Button offButton;
+    Button forwardButton;
+    Button backButton;
     Button leftButton;
     Button rightButton;
     Button streightButton;
@@ -75,49 +82,63 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     void initializeUI(){
-        onButton = (Button) findViewById(R.id.on);
-        offButton = (Button) findViewById(R.id.off);
+        forwardButton = (Button) findViewById(R.id.forward);
+        backButton = (Button) findViewById(R.id.back);
         leftButton = (Button) findViewById(R.id.left);
         rightButton = (Button) findViewById(R.id.right);
         streightButton = (Button) findViewById(R.id.straight);
         speedBar = (SeekBar) findViewById(R.id.speed);
 
-        onButton.setOnClickListener(new View.OnClickListener() {
+        speedBar.setMax(255);
+
+        forwardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                create.connectionThread.write("A".getBytes());
+                dir = Direction.FORWARD;
+                sendMessage();
+//                create.connectionThread.write("A".getBytes());
             }
         });
-        offButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                create.connectionThread.write("B".getBytes());
+                dir = Direction.BACK;
+                sendMessage();
+//                create.connectionThread.write("B".getBytes());
             }
         });
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                create.connectionThread.write("L".getBytes());
+                steer = Steering.LEFT;
+                sendMessage();
+//                create.connectionThread.write("L".getBytes());
             }
         });
         rightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                create.connectionThread.write("R".getBytes());
+                steer = Steering.RIGHT;
+                sendMessage();
+//                create.connectionThread.write("R".getBytes());
             }
         });
         streightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                create.connectionThread.write("S".getBytes());
+                steer = Steering.STRAIGHT;
+                sendMessage();
+//                create.connectionThread.write("S".getBytes());
             }
         });
 
         speedBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                byte[] value = BigInteger.valueOf((long)progress).toByteArray();
-                create.connectionThread.write(value);
+//                byte[] value = BigInteger.valueOf((long)progress).toByteArray();
+                speed = progress;
+                sendMessage();
+//                create.connectionThread.write(value);
             }
 
             @Override
@@ -131,16 +152,24 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         });
     }
-    public void onPause(){
-        super.onPause();
-        create.cancel();
-        create = null;
+    void sendMessage(){
+        if(null != create && null != create.connectionThread) {
+            create.connectionThread.write(this.getCommand().getBytes());
+        }
     }
-    public void onResume(){
-        super.onResume();
-        create = new ConnectionCreationThread(device, BluetoothAdapter.getDefaultAdapter());
-        create.start();
+    private String getCommand(){
+        return "Dir:"+dir.name()+"|Speed:"+String.valueOf(speed)+"|Steer:"+steer.name()+"|\n";
     }
+//    public void onPause(){
+//        super.onPause();
+//        create.cancel();
+//        create = null;
+//    }
+//    public void onResume(){
+//        super.onResume();
+//        create = new ConnectionCreationThread(device, BluetoothAdapter.getDefaultAdapter());
+//        create.start();
+//    }
     protected void onDestroy(){
         if(null != create){
             create.cancel();
--- sketch_oct30a/sketch_oct30a.ino
+++ sketch_oct30a/sketch_oct30a.ino
@@ -10,24 +10,17 @@ int dir1PinA = 13;
 int dir2PinA = 12;
 int speedPinA = 10;
 
-
-unsigned long time;
-int speed;
-int dir;
+String currentSteering = "";
 Servo steer;
 
 void setup() {
-  steer.attach(4);
+//  steer.attach(4);
   steer.write(90);
   Serial.begin(38400);
   
   pinMode(dir1PinA, OUTPUT);
   pinMode(dir2PinA, OUTPUT);
   pinMode(speedPinA, OUTPUT);
-
-  time = millis();
-  speed = 255;
-  dir = 1;
   
   Serial.println("Starting config");
   mySerial.begin(BLUETOOTH_SPEED);
@@ -56,6 +49,7 @@ void setup() {
 
   Serial.println("Done!");
 }
+
 void waitForResponse() {
     delay(1000);
     while (mySerial.available()) {
@@ -65,36 +59,86 @@ void waitForResponse() {
 }
 void loop() {
   if(mySerial.available() > 0){
-  char command = mySerial.read();
-   Serial.println(command);
+  String command = mySerial.readStringUntil('\n');
+  char cmd[command.length()];
+  char * ptr;
+  Serial.print("String Length:");
+  Serial.println(command.length());
+  command.toCharArray(cmd, command.length());
+  
+  ptr = strtok(cmd,"|");
+  String direction = ptr;
+  ptr = strtok(NULL, "|");
+  String speed = ptr;
+  ptr = strtok(NULL, "|");
+  String steering = ptr;
+  Serial.println(direction);
+  Serial.println(speed);
+  Serial.println(steering);
+  
+  speed = speed.substring(6);
+  int speedValue = speed.toInt();
+  Serial.println(speedValue);
+  if(speedValue > 0){
+    if(direction.equals("Dir:BACK")){
+      Serial.println("Go Back");
+        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir2PinA, HIGH);
+    }else if(direction.equals("Dir:FORWARD")){
+      Serial.println("Go Froward");
+        digitalWrite(dir1PinA, HIGH);
+        digitalWrite(dir2PinA, LOW);    
+    }
+    analogWrite(speedPinA, speedValue);
+  } else {
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, LOW);
+    analogWrite(speedPinA, 0);
+  }
+  if(!currentSteering.equals(steering)){
+    if(steering.equals("Steer:LEFT")){
+      Serial.println("Go left");
+      steer.write(20);
+    } else if(steering.equals("Steer:RIGHT")){
+      Serial.println("Go right");
+      steer.write(160);
+    } else if(steering.equals("Steer:STRAIGHT")){
+      Serial.println("Go straight");
+      steer.write(90);
+    }
+    currentSteering = steering;
+  }
+  Serial.println("Command:" + command);
+  
+  }
+   
 //  digitalWrite(dir1PinA, HIGH);
 //      digitalWrite(dir2PinA, LOW);
 //      analogWrite(speedPinA, speed);
-  switch(command){
-    case 'A':
-      digitalWrite(dir1PinA, HIGH);
-      digitalWrite(dir2PinA, LOW);
-      analogWrite(speedPinA, speed);
-        Serial.println(command);
-      break;
-    case 'B':
-      digitalWrite(dir1PinA, LOW);
-      digitalWrite(dir2PinA, LOW);
-      analogWrite(speedPinA, 0);
-        Serial.println(command);
-      break;
-    case 'L': 
-      steer.write(20);
-      break;
-    case 'R':
-      steer.write(160);
-      break;
-    case 'S':
-      steer.write(90);
-      break;
-  }
-  }
- 
+//  switch(command){
+//    case 'A':
+//      digitalWrite(dir1PinA, HIGH);
+//      digitalWrite(dir2PinA, LOW);
+//      analogWrite(speedPinA, speed);
+//        Serial.println(command);
+//      break;
+//    case 'B':
+//      digitalWrite(dir1PinA, LOW);
+//      digitalWrite(dir2PinA, LOW);
+//      analogWrite(speedPinA, 0);
+//        Serial.println(command);
+//      break;
+//    case 'L': 
+//      steer.write(20);
+//      break;
+//    case 'R':
+//      steer.write(160);
+//      break;
+//    case 'S':
+//      steer.write(90);
+//      break;
+//  }
+// 
   // set direction
 //  if (1 == dir) {
 //    steer.write(20);
